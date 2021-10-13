304365a787a36c30fa586472840e46c3894b78f5
Aleksei-Mac <alexsanatov@gmail.com>
2021-10-13 18:25:04 +0300

    fix: исправил информация в блоке о себе

--- diploma-app/src/components/Main/AboutMe/AboutMe.js
+++ diploma-app/src/components/Main/AboutMe/AboutMe.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './AboutMe.css';
-import StudentPicture from '../../../images/AboutMeImage.png';
+import myPicture from '../../../images/My-foto.jpg';
 
 function AboutMe() {
   return (
@@ -11,8 +11,7 @@ function AboutMe() {
         <div className="about__text-side">
           <h3 className="about__name">Алексей</h3>
           <h4 className="about__developer">Фронтенд-разработчик, 22 лет</h4>
-          <p className="about__paragraph">Я родился и живу в Рига У меня есть жена
-            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <p className="about__paragraph">Я родился и живу в Риге. Развиваюсь как Фронтенд разработчик, закончил факультет Веб-разработки в Яндекс Практикуме. И начинаю свой путь в IT. Инетересуюсь новыми технологиями, особенно в машинной сфере. Играю в футбол и теннис, это мои основные хобби. </p>
           <ul className="about__links">
             <li className="about__list-item">
               <a className="about__link" rel={'noreferrer'} href="https://www.facebook.com/aleksej.sanatov/" target={'_blank'}>Facebook</a>
@@ -23,7 +22,7 @@ function AboutMe() {
           </ul>
         </div>
         <div className="about__image-side">
-          <img src={StudentPicture} alt="Фото студента" className="about__image"/>
+          <img src={myPicture} alt="Фото студента" className="about__image"/>
         </div>
       </div>
     </div>
